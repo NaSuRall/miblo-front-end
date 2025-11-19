b52453040037444dcad33671a9bcc29a07feb430
samuel <samuel@Mac-CF-23-002.local>
2025-11-19 09:03:00 +0100

    MBL ADD : formDepoMoney components

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
 import Dashboard from "./components/dashboard.jsx"
+import FormDepoMoney from "./components/FormDepoMoney.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/*" element={<App />} />
       <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="/depositmoney" element={<FormDepoMoney/>} />
     </Routes>
   </BrowserRouter>
 );
