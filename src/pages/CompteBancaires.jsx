d6f693848c74810d56334d3bbfa011ceb2b04c18
Romain Poulain <romain.poulain15@gmail.com>
2025-11-19 23:07:25 +0100

    MBL ADD : Add Dashboard Components Adapte to react Rooter for Implement NavBar change main.jsx for component of navbar

--- src/pages/CompteBancaires.jsx
+++ src/pages/CompteBancaires.jsx
@@ -4,6 +4,7 @@ import BankAccountList from '../components/BankAccount/BankAccountListe'
 import AddAccountButton from '../components/BankAccount/AddAccountButton'
 import { getBankAccounts } from "../services/api/bankAccountService";
 
+
 function bankAccount() {
 
     const [accounts, setAccounts] = useState([]);
@@ -22,23 +23,12 @@ function bankAccount() {
     }, []); 
 
   return (
-    <main className="flex flex-row w-full h-screen  gap-5 p-3 " style={{backgroundColor: "var(--background-color)"}}>
-        <div className='flex flex-col w-[15%] h-full  items-center justify-center rounded-xl' style={{backgroundColor: "var(--bacground-card)"}}>
-            <h2 className='' style={{color: "var(--text-color)"}}>Navbar</h2>
-        </div>
-
-        <div className='flex flex-col items-center justify-center w-full h-full rounded-xl p-3 overflow-scroll' style={{backgroundColor: "var(--bacground-card)"}}>
-            
+        <div className='flex flex-col items-center justify-center w-full h-full rounded-xl p-3 ' style={{backgroundColor: "var(--bacground-card)"}}>
             <div className='flex flex-col w-full p-6 '>
-              <AddAccountButton  
-                accounts={accounts}
-                
-              />
+              <AddAccountButton accounts={accounts} />
             </div>
-
             <BankAccountList accounts={accounts} />
         </div>
-    </main>
   )
 }
 
